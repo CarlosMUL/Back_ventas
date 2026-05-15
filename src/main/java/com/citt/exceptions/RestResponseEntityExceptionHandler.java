54bd0fd8e2cfbc8ee71f37c82178d7a69ade55e0
Carlos Andres Muñoz Leyton <carlo.munozl@duocuc.cl>
2026-05-15 03:17:01 -0400

    ci: activar pipeline deploy

--- src/main/java/com/citt/exceptions/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/citt/exceptions/RestResponseEntityExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
+/*actualizacion */
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
